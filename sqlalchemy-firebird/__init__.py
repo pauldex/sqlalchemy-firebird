b5c5de6124d8d836bfab891414fb31c8dbcd3192
Paul <pgraves-deslauriers@esri.com>
2019-12-26 13:10:32 -0800

    Initial Cut
    Starting from sqlalchemy-access and sqlalchemy-master

---
+++ sqlalchemy-firebird/__init__.py
@@ -0,0 +1,43 @@
+# firebird/__init__.py
+# Copyright (C) 2005-2019 the SQLAlchemy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of SQLAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+# This package is the original support for firebird from sqlalchemy-master
+
+from sqlalchemy.dialects.firebird.base import BIGINT
+from sqlalchemy.dialects.firebird.base import BLOB
+from sqlalchemy.dialects.firebird.base import CHAR
+from sqlalchemy.dialects.firebird.base import DATE
+from sqlalchemy.dialects.firebird.base import FLOAT
+from sqlalchemy.dialects.firebird.base import NUMERIC
+from sqlalchemy.dialects.firebird.base import SMALLINT
+from sqlalchemy.dialects.firebird.base import TEXT
+from sqlalchemy.dialects.firebird.base import TIME
+from sqlalchemy.dialects.firebird.base import TIMESTAMP
+from sqlalchemy.dialects.firebird.base import VARCHAR
+from . import base  # noqa
+from . import fdb  # noqa
+from . import kinterbasdb  # noqa
+
+
+base.dialect = dialect = fdb.dialect
+
+__all__ = (
+    "SMALLINT",
+    "BIGINT",
+    "FLOAT",
+    "FLOAT",
+    "DATE",
+    "TIME",
+    "TEXT",
+    "NUMERIC",
+    "FLOAT",
+    "TIMESTAMP",
+    "VARCHAR",
+    "CHAR",
+    "BLOB",
+    "dialect",
+)
---
+++ test/test_suite.py
@@ -0,0 +1,132 @@
+from sqlalchemy.testing.suite import *
+
+# This is the original sqlalchemy-access test suite
+
+from sqlalchemy.testing.suite import ComponentReflectionTest as _ComponentReflectionTest
+from sqlalchemy.testing.suite import ExpandingBoundInTest as _ExpandingBoundInTest
+from sqlalchemy.testing.suite import InsertBehaviorTest as _InsertBehaviorTest
+from sqlalchemy.testing.suite import IntegerTest as _IntegerTest
+from sqlalchemy.testing.suite import LikeFunctionsTest as _LikeFunctionsTest
+from sqlalchemy.testing.suite import NumericTest as _NumericTest
+from sqlalchemy.testing.suite import OrderByLabelTest as _OrderByLabelTest
+from sqlalchemy.testing.suite import TableDDLTest as _TableDDLTest
+#others???
+
+
+class ComponentReflectionTest(_ComponentReflectionTest):
+    @classmethod
+    def test_get_noncol_index_pk(cls):
+        # This test fails because Access automatically creates a unique *index* (not constraint) on the primary key.
+        # The test is expecting to see just one index (on a non-PK column) but it is seeing two.
+        return
+
+    @classmethod
+    def test_get_unique_constraints(cls):
+        # Access barfs on DDL trying to create a constraint named "i.have.dots"
+        return
+
+    @classmethod
+    def test_nullable_reflection(cls):
+        # Access ODBC implementation of the SQLColumns function reports that a column is nullable even when it is not
+        return
+
+class ExpandingBoundInTest(_ExpandingBoundInTest):
+    @classmethod
+    def test_null_in_empty_set_is_false(cls):
+        """ Access SQL can't do CASE ... WHEN, but this test would pass if we re-wrote the query to be
+                SELECT (n = 1) AS result
+                FROM
+                    (
+                        SELECT COUNT(*) AS n FROM USysSQLAlchemyDUAL
+                        WHERE NULL IN (SELECT NULL FROM USysSQLAlchemyDUAL WHERE 1=0)
+                    )
+        """
+        return
+
+
+class InsertBehaviorTest(_InsertBehaviorTest):
+    @classmethod
+    def test_empty_insert(cls):
+        # bypass this test because Access ODBC fails with
+        # [ODBC Microsoft Access Driver] Syntax error in INSERT INTO statement.
+        return
+
+
+class IntegerTest(_IntegerTest):
+    @classmethod
+    def test_huge_int(cls):
+        # bypass this test because Access ODBC fails with
+        # [ODBC Microsoft Access Driver]Optional feature not implemented.
+        return
+
+
+class LikeFunctionsTest(_LikeFunctionsTest):
+    # Access SQL doesn't do ESCAPE
+    @classmethod
+    def test_contains_autoescape(cls):
+        return
+
+    @classmethod
+    def test_contains_autoescape_escape(cls):
+        return
+
+    @classmethod
+    def test_contains_escape(cls):
+        return
+
+    @classmethod
+    def test_endswith_autoescape(cls):
+        return
+
+    @classmethod
+    def test_endswith_autoescape_escape(cls):
+        return
+
+    @classmethod
+    def test_endswith_escape(cls):
+        return
+
+    @classmethod
+    def test_startswith_autoescape(cls):
+        return
+
+    @classmethod
+    def test_startswith_autoescape_escape(cls):
+        return
+
+    @classmethod
+    def test_startswith_escape(cls):
+        return
+
+
+class NumericTest(_NumericTest):
+    @classmethod
+    def test_decimal_coerce_round_trip(cls):
+        # bug in Access SQL: "SELECT ? AS anon_1 ..." returns rubbish with a decimal.Decimal parameter value
+        # https://github.com/mkleehammer/pyodbc/issues/624
+        return
+
+    @classmethod
+    def test_decimal_coerce_round_trip_w_cast(cls):
+        # bug in Access SQL: "SELECT ? AS anon_1 ..." returns rubbish with a decimal.Decimal parameter value
+        # https://github.com/mkleehammer/pyodbc/issues/624
+        return
+
+
+class OrderByLabelTest(_OrderByLabelTest):
+    @classmethod
+    def test_composed_multiple(cls):
+        # SELECT statement too complex for Access SQL
+        # [ODBC Microsoft Access Driver] Reserved error (-1001); there is no message for this error.
+        return
+
+
+class TableDDLTest(_TableDDLTest):
+    # Access doesn't do schemas
+    @classmethod
+    def test_create_table_schema(cls):
+        return
+
+    @classmethod
+    def test_underscore_names(cls):
+        return
