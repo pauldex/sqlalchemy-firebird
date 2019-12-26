5036785bf65d355212d64959c992304f4d2cd898
Paul <pgraves-deslauriers@esri.com>
2019-12-26 13:26:57 -0800

    Change testing from Access to Firebird

--- test/conftest.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.dialects import registry
-import pytest
-
-# registry.register("access.pyodbc", "sqlalchemy_access.pyodbc", "AccessDialect_pyodbc")
-registry.register("???", "???", "???")
-
-pytest.register_assert_rewrite("sqlalchemy.testing.assertions")
-
-from sqlalchemy.testing.plugin.pytestplugin import *
