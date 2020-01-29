0aa31374f74665a4cbda1c0679ff14b07114e08b
Paul <pgraves-deslauriers@esri.com>
2020-01-29 10:40:19 -0800

    Change from Markdown to Restructured Text documentation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ v = open(os.path.join(os.path.dirname(__file__), 'sqlalchemy_firebird', '__init_
 VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
 v.close()
 
-readme = os.path.join(os.path.dirname(__file__), 'README.md')
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 
 setup(name='sqlalchemy_firebird',
