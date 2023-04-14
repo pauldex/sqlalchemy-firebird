3c0df3a5fd926ed01b1e6e2c1c8a56f6dc8431ef
F.D.Castel <fdcastel@gmail.com>
2023-04-14 19:42:39 -0300

    Disables test_enotation_decimal_large.

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,1 +1,12 @@
 from sqlalchemy.testing.suite import *  # noqa: F401, F403
+
+
+from sqlalchemy.testing.suite import NumericTest as _NumericTest
+
+
+class NumericTest(_NumericTest):
+    @testing.skip("firebird")
+    def test_enotation_decimal_large(self):
+        # Failing with
+        # sqlalchemy.exc.DatabaseError: (fdb.fbcore.DatabaseError) ('Cursor.fetchone:\n- SQLCODE: -804\n- Incorrect values within SQLDA structure\n- empty pointer to data\n- at SQLVAR index 0', -804, 335544713)        # Table unknown
+        return
