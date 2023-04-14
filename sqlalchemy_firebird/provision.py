b378b8b118078d81ad9688b71f35025eca34fce9
F.D.Castel <fdcastel@gmail.com>
2023-04-14 19:32:30 -0300

    Tests workaround: Commit DDL commands before DML ones.

--- sqlalchemy_firebird/provision.py
+++ sqlalchemy_firebird/provision.py
@@ -1,4 +1,8 @@
+from sqlalchemy import Engine, event
+from sqlalchemy.schema import CreateTable, DropTable
 from sqlalchemy.testing.provision import temp_table_keyword_args
+
+import logging
 
 
 @temp_table_keyword_args.for_db("firebird")
@@ -7,3 +11,25 @@ def _firebird_temp_table_keyword_args(cfg, eng):
         "prefixes": ["GLOBAL TEMPORARY"],
         "firebird_on_commit": "PRESERVE ROWS",
     }
+
+
+HAS_DDL_PENDING = False
+
+
+@event.listens_for(Engine, "before_execute")
+def receive_before_execute(connection, statement, *arg):
+    global HAS_DDL_PENDING
+
+    if isinstance(statement, CreateTable) or isinstance(statement, DropTable):
+        HAS_DDL_PENDING = True
+        return
+
+    if HAS_DDL_PENDING:
+        log = logging.getLogger("sqlalchemy.firebird.provision")
+        log.warning("Flushing DDL, conn = %s", str(connection))
+
+        # Using Connection protected methods here because the public ones cause errors with TransactionManager
+        connection._commit_impl()
+        connection._begin_impl(connection._transaction)
+
+        HAS_DDL_PENDING = False
