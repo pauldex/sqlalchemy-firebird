e6ade6025a9de33acbea60c3ef6f0449992edea4
F.D.Castel <fdcastel@gmail.com>
2023-04-24 20:10:25 -0300

    More strict test skips.

--- test/test_suite.py
+++ test/test_suite.py
@@ -5,8 +5,7 @@ from sqlalchemy.testing.suite import *  # noqa: F401, F403
 from sqlalchemy.testing.suite import CTETest as _CTETest
 
 from sqlalchemy.testing.suite import CompoundSelectTest as _CompoundSelectTest
-from sqlalchemy.testing.suite import DateTimeTZTest as _DateTimeTZTest
-from sqlalchemy.testing.suite import InsertBehaviorTest as _InsertBehaviorTest
+sfrom sqlalchemy.testing.suite import InsertBehaviorTest as _InsertBehaviorTest
 from sqlalchemy.testing.suite import NumericTest as _NumericTest
 from sqlalchemy.testing.suite import RowCountTest as _RowCountTest
 
@@ -20,25 +19,29 @@ class CTETest(_CTETest):
 
 @pytest.mark.skip(reason="These tests hangs in Firebird 2.5.")
 class InsertBehaviorTest(_InsertBehaviorTest):
-    pass
+    @testing.skip("firebird=2", reason="This test hangs in Firebird 2.0")
+    def test_insert_from_select(self):
+        pass
 
 
 @pytest.mark.skip(reason="These tests hangs in Firebird 3.0.")
 class CompoundSelectTest(_CompoundSelectTest):
-    pass
-
-
-@pytest.mark.skip(reason="These tests hangs in Firebird 4.0.")
-class DateTimeTZTest(_DateTimeTZTest):
-    pass
+    @testing.skip("firebird=3", reason="This test hangs in Firebird 3.0")
+    def test_limit_offset_aliased_selectable_in_unions(self):
+        pass
 
 
 class NumericTest(_NumericTest):
-    @testing.skip("firebird", reason="This test hangs in Firebird 4.0")
+    @testing.skip(
+        "firebird+fdb=4", reason="This test hangs in Firebird 4.0 with fdb"
+    )
     def test_enotation_decimal_large(self):
         return
 
 
-@pytest.mark.skip(reason="These tests hangs in Firebird 4.0.")
 class RowCountTest(_RowCountTest):
-    pass
+    @testing.skip(
+        "firebird+fdb=4", reason="This test hangs in Firebird 4.0 with fdb"
+    )
+    def test_update_rowcount2(self):
+        pass
