3af1b3befd09779e05a765307374699f70fb49ce
Paul Graves-DesLauriers <paul@dexmicro.com>
2023-05-07 19:56:39 -0700

    Additional kinterbase support removal

--- sqlalchemy_firebird/fdb.py
+++ sqlalchemy_firebird/fdb.py
@@ -13,38 +13,14 @@ from sqlalchemy import util
 from sqlalchemy import __version__ as sqla_version
 from re import match
 from .base import FBDialect
-from .base import FBExecutionContext
-
-
-class FBExecutionContext_fdb(FBExecutionContext):
-    @property
-    def rowcount(self):
-        if self.execution_options.get(
-            "enable_rowcount", self.dialect.enable_rowcount
-        ):
-            return self.cursor.rowcount
-        else:
-            return -1
 
 
 class FBDialect_fdb(FBDialect):
     name = "firebird.fdb"
     driver = "fdb"
-    supports_sane_rowcount = False
     supports_sane_multi_rowcount = False
     supports_statement_cache = True
     supports_native_decimal = True
-    execution_ctx_cls = FBExecutionContext_fdb
-    using_dialect_3 = False
-
-    def __init__(self, enable_rowcount=True, retaining=False, **kwargs):
-        super(FBDialect_fdb, self).__init__(
-            enable_rowcount=enable_rowcount, retaining=retaining, **kwargs
-        )
-        self.enable_rowcount = enable_rowcount
-        self.retaining = retaining
-        if enable_rowcount:
-            self.supports_sane_rowcount = True
 
     if sqla_version < "2":
 
@@ -57,12 +33,6 @@ class FBDialect_fdb(FBDialect):
         @classmethod
         def import_dbapi(cls):
             return __import__("fdb")
-
-    def do_rollback(self, dbapi_connection):
-        dbapi_connection.rollback(self.retaining)
-
-    def do_commit(self, dbapi_connection):
-        dbapi_connection.commit(self.retaining)
 
     def create_connect_args(self, url):
         opts = url.translate_connect_args(username="user")
@@ -78,49 +48,38 @@ class FBDialect_fdb(FBDialect):
     def _get_server_version_info(self, connection):
         """Get the version of the Firebird server used by a connection.
 
-        Returns a tuple of (`major`, `minor`, `build`), three integers
-        representing the version of the attached server.
+        Returns a tuple of (`major`, `minor`, `build`, `server_name`), three integers representing the version
+        of the attached server and a string representing the name of the server type (firebird or interbase).
         """
+
         # This is the simpler approach (the other uses the services api),
         # that for backward compatibility reasons returns a string like
         #   LI-V6.3.3.12981 Firebird 2.0
         # where the first version is a fake one resembling the old
         # Interbase signature.
+        def parse_version_info0(version):
+            m = match(
+                r"\w+-[V|T](\d+)\.(\d+)\.(\d+)\.(\d+)( \w+ (\d+)\.(\d+))?",
+                version,
+            )
+            if not m:
+                raise AssertionError(
+                    "Could not determine version from string '%s'" % version
+                )
+
+            if m.group(5) is not None:
+                return tuple([int(x) for x in m.group(6, 7, 4)] + ["firebird"])
+            else:
+                return tuple(
+                    [int(x) for x in m.group(1, 2, 3)] + ["interbase"]
+                )
+
         isc_info_firebird_version = 103
         fbconn = connection.connection
 
         version = fbconn.db_info(isc_info_firebird_version)
 
-        return self._parse_version_info(version)
-
-    def _parse_version_info(self, version):
-        m = match(
-            r"\w+-[V|T](\d+)\.(\d+)\.(\d+)\.(\d+)( \w+ (\d+)\.(\d+))?", version
-        )
-        if not m:
-            raise AssertionError(
-                "Could not determine version from string '%s'" % version
-            )
-
-        if m.group(5) is not None:
-            return tuple([int(x) for x in m.group(6, 7, 4)] + ["firebird"])
-        else:
-            return tuple([int(x) for x in m.group(1, 2, 3)] + ["interbase"])
-
-    def is_disconnect(self, e, connection, cursor):
-        if isinstance(
-            e, (self.dbapi.OperationalError, self.dbapi.ProgrammingError)
-        ):
-            msg = str(e)
-            return (
-                "Error writing data to the connection" in msg
-                or "Unable to complete network request to host" in msg
-                or "Invalid connection state" in msg
-                or "Invalid cursor state" in msg
-                or "connection shutdown" in msg
-            )
-        else:
-            return False
+        return parse_version_info0(version)
 
 
 dialect = FBDialect_fdb
